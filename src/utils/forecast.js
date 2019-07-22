68c40e58bb4e98d6184223fc73b723d4c8dc51c7
vojtechjirasek <23-6520-1@plus4u.net>
2019-07-22 16:18:11 +0200

    challenge add max temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/870b6b70a8ab4e9682df1aa6cfb72496/' + latitude + ',' + longitude + '?units=si&lang=cs'
+    const url = 'https://api.darksky.net/forecast/870b6b70a8ab4e9682df1aa6cfb72496/' + latitude + ',' + longitude + '?units=si&temperatureMax'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' Je přesně ' + body.currently.temperature + ' stupňů celsia. Také je ' + body.currently.precipProbability + '% šance deště.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.' + ' Maximum temperature is ' + body.daily.data[0].temperatureMax)
         }
     })
 }
