1ad9c1c3df32bf329062cd2483e1d114cdd9f9ff
vojtechjirasek <23-6520-1@plus4u.net>
2019-07-22 16:19:56 +0200

    oprava

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.' + ' Maximum temperature is ' + body.daily.data[0].temperatureMax)
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.' + ' Maximum temperature is ' + body.daily.data[0].temperatureMax + '.')
         }
     })
 }
